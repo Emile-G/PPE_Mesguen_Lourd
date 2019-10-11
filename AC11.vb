19a05b94f417bfadcb85a9e9e515fb8b50cd83a3
Sky-moon <egardent.pro@gmail.com>
2019-10-11 11:59:36 +0200

    Update AC11.vb

--- AC11.vb
+++ AC11.vb
@@ -1,12 +1,17 @@
 ﻿Public Class AC11
+    Dim myConnection As New Odbc.OdbcConnection
+    Dim myCommand As New Odbc.OdbcCommand
+    Dim myReader As Odbc.OdbcDataReader
+    Dim myAdapter As Odbc.OdbcDataAdapter
+    Dim myBuilder As Odbc.OdbcCommandBuilder
+    Dim connString As String
 
     Private Sub AC11_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
         'TODO: cette ligne de code charge les données dans la table 'DataSet1.DataTable1'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
         Me.DataTable1TableAdapter.Fill(Me.DataSet1.DataTable1)
-        'TODO: cette ligne de code charge les données dans la table 'DataSet1.DataTable1'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
-        Me.DataTable1TableAdapter.Fill(Me.DataSet1.DataTable1)
-        'TODO: cette ligne de code charge les données dans la table 'DataSet1.DataTable1'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
-        Me.DataTable1TableAdapter.Fill(Me.DataSet1.DataTable1)
+        connString = "DSN=CNXORA_Mesguen;Uid=U_Mesguen;Pwd=estran;"
+        myConnection.ConnectionString = connString
+        myConnection.Open()
 
     End Sub
 
